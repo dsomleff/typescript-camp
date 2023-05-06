59ff849a1d55baac9762b569be7b87d0221bd293
somleff <somleff@gmail.com>
2023-05-06 15:20:28 +0200

    Implicit Type Checks

--- maps/src/CustomMap.ts
+++ maps/src/CustomMap.ts
@@ -1,6 +1,11 @@
-/// <reference types="@types/google.maps" />
-import { User } from "./User";
-import { Company } from "./Company";
+// Instructions to every other class
+// on how they can be an argument to 'addMarker'
+interface Mappable {
+  location: {
+    lat: number;
+    lng: number;
+  };
+}
 
 export class CustomMap {
   private googleMap: google.maps.Map;
@@ -18,7 +23,7 @@ export class CustomMap {
     );
   }
 
-  addMarker(mappable: User | Company): void {
+  addMarker(mappable: Mappable): void {
     new google.maps.Marker({
       map: this.googleMap,
       position: {
@@ -27,14 +32,4 @@ export class CustomMap {
       },
     });
   }
-
-  // addCompanyMarker(company: Company): void {
-  //   new google.maps.Marker({
-  //     map: this.googleMap,
-  //     position: {
-  //       lat: company.location.lat,
-  //       lng: company.location.lng
-  //     }
-  //   });
-  // }
 }
